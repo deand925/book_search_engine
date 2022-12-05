5eae86be440abb8ce8057b9af872a4a97ba2053c
deand925 <97762768+deand925@users.noreply.github.com>
2022-12-05 12:36:13 -0800

    updated typeDef with graphql info

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,36 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type: User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [book]
+    }
+
+    type: Book {
+        _id: ID
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+      }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(authors: [String], title: String, bookId: String!, image: String, link: String, description: String): User
+    }
+`
